2e6b6d8695041833bbc0079e0dac59fb598ecdb7
israfil <israfil3019@gmail.com>
2021-03-20 13:24:40 +0300

    add switch-case version of examples

--- third-sprint/pathselect.js
+++ third-sprint/pathselect.js
@@ -16,13 +16,31 @@ console.log(pathInfo);
 
 // ! ternary
 pathInfo =
-  pathSelected === '"ad'
+  pathSelected === 'ad'
     ? 'You are going to manage infrastructure of cloud servers'
-    : pathSelected === '"ds'
+    : pathSelected === 'ds'
     ? 'You will manipulate high volume of data to obtain meaningful results'
-    : pathSelected === '"fs'
+    : pathSelected === 'fs'
     ? 'You will build awesome web applications'
     : 'Invalid selection';
 console.log(pathInfo);
 
 // ! switch case
+
+pathInfo;
+switch(pathSelected) {
+  case "ad" :
+    pathInfo = "Good to Go";
+    break;
+  case "fs" :
+    pathInfo = "Excellent";
+    break;
+  case "ds" : 
+    pathInfo = "Why not?";
+    break;
+  default :
+    pathInfo= "wrong selection";
+    break;
+}
+console.log(pathInfo);
+
--- third-sprint/session.js
+++ third-sprint/session.js
@@ -10,4 +10,27 @@ if (day === 'monday') {
 } else if (day === 'saturday') {
   sessionInfo = 'There is single session';
 }
+console.log(sessionInfo);
+
+
 // ! switch case
+
+sessionInfo;
+switch(day) {
+  case "monday":
+    sessionInfo = "There is just one on Monday";
+    break;
+  case "wednesday":
+    sessionInfo = "there is just one on Wednesday";
+    break;
+  case "thursday":
+    sessionInfo = "there is just one on Thursday";    
+    break;
+  case "saturday":
+    sessionInfo = "there is two session on Saturday";
+    break;  
+  default:
+    sessionInfo = "Wrong selection";
+    break;
+}
+console.log(sessionInfo);
