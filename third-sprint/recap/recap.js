da2d8a62ecfb7e4fc5583449308b219ffa968804
israfil <israfil3019@gmail.com>
2021-03-24 17:46:13 +0300

    add recap

---
+++ third-sprint/recap/recap.js
@@ -0,0 +1,17 @@
+// document.write("Hello world!")
+// console.log("our source")
+// alert("selam")
+// confirm("Are we good?")
+// console.table("selam", " nasılsın")
+// console.error("Hata")
+// console.warn("uyarı")
+// console.info("bilginiz için")
+// var => let, const
+// let a = 5
+// a =6
+// const b = "selam"
+// c = true
+
+// let x
+// let y = null
+// console.log(typeof y)
