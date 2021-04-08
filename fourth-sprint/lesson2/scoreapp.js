916e4c125c157fcac8e7a098d473c22849666266
israfil <israfil3019@gmail.com>
2021-04-08 17:07:32 +0300

    finish lesson notes

--- fourth-sprint/lesson2/scoreapp.js
+++ fourth-sprint/lesson2/scoreapp.js
@@ -8,3 +8,29 @@ const span = document.querySelectorAll("span");
 console.log(winScore);
 console.log(spanPlayer1.textContent);
 
+const btnPlayer1 = document.getElementById("btnPl1");
+
+// btnPlayer1.addEventListener("click", player1Click);
+
+// function player1Click() {
+//     scores[0] == 1;
+//     spanPlayer1.textContent = scores[0];
+// }
+
+const btnPlayer2 = document.getElementById("btnPl2");
+
+// btnPlayer1.addEventListener("click", player2Click);
+
+// function player2Click() {
+//     scores[1] == 1;
+//     spanPlayer2.textContent = scores[1];
+// }
+
+btnPlayer1.addEventListener("click", increaseCounter);
+btnPlayer2.addEventListener("click", increaseCounter);
+
+function increaseCounter(event) {
+    const pl = this.id.slice(-1) - 1;
+    scores[pl]++;
+    spans[pl].textContent = scores[pl];
+}
