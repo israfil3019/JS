0c0ec6795f81ed271554ff78d6cd432a5d45f29f
israfil <israfil3019@gmail.com>
2021-04-08 12:50:54 +0300

    add js lesson notes

---
+++ fourth-sprint/lesson2/scoreapp.js
@@ -0,0 +1,10 @@
+const score = [0, 0];
+
+const winScore = Number(document.getElementById("winscore").value);
+const spanPlayer1 = document.getElementById("player1");
+const spanPlayer2 = document.getElementById("player2");
+const span = document.querySelectorAll("span");
+
+console.log(winScore);
+console.log(spanPlayer1.textContent);
+
