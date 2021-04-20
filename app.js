80de4ca13dd9eb023843807a1a5de87338b09ef0
israfil <israfil3019@gmail.com>
2021-04-20 18:10:35 +0300

    create dummy file for practice question

---
+++ app.js
@@ -0,0 +1,51 @@
+// function spec_opr(...numbers) {
+//     let sum = 0;
+//     for (const number of numbers) {
+//       number % 2 === 0 ? sum += number / 2 : sum += number * 3;
+//     }
+//     console.log(sum);
+//   }
+// spec_opr(1,2,3,4);
+
+
+// const d = Array.from("Clarusway");
+// console.log(d);
+
+// const e = Array.from([3, 5, 7], n => n * n);
+// console.log(e);
+
+// const children = [12,25,18,26];
+// const fillfunction = (age) =>{
+//   return age >=15;
+// }
+
+// console.log(children.find(fillfunction));
+
+// console.log(children.findIndex(fillfunction));
+
+
+// function palindrome(myword){
+//   let palindrome = myword.split("").reverse().join("");
+//   console.log(palindrome);
+//   (palindrome == myword) ? console.log(`${myword} is palindrome`) : console.log(`${myword} is not palindrome`);
+// }
+// let myword = prompt("Enter your word: ");
+// palindrome(myword);
+
+// for(let i = 6; i <= 1000; i++){
+//   let sum = 0;
+//   for(let j = 1; j < i; j++){
+//     if(i % j == 0) sum += j;
+//   }
+//   (i == sum) ? console.log(i) : false;
+// }
+
+// const arr = [ [[1,2], [-1,-2]], ["a", "b"] ];
+// const myArr = arr.toString().split(",");
+// console.log(myArr);
+
+
+// const operations = ["5 + 6", "2 * 3", "8 / 4", "4 - 5"];
+// for (i=0; i<operations.length; i++){
+//     console.log(operations[i]);
+// }
