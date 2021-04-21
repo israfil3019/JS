15bde8eff1d6531e7928b95889aa0d2e67a84a5a
israfil <israfil3019@gmail.com>
2021-04-21 18:08:01 +0300

    add object some modify methods

---
+++ fifth-sprint/lesson2/app.js
@@ -0,0 +1,60 @@
+/* eslint-disable max-classes-per-file */
+// 'use strict';
+// // creating objects
+// const person = {
+//   firstName: 'Barry',
+//   lastName: 'Mitchell',
+//   birthYear: 1977,
+//   skills: ['JS', 'AWS', 'Dockers', 'Python'],
+//   employed: true,
+
+//   2021: 'Clarusway FS',
+//   true: 'is it working',
+// };
+
+// const teacher = {};
+
+// teacher.fullName = 'Barry Mitchell';
+// teacher.age = 44;
+
+// accessing values
+// console.log(person.firstName);
+// console.log(person.birthYear);
+
+// console.log(teacher.fullName);
+
+// console.log(person['2021']);
+// console.log(person.true);
+
+// const year = 2021;
+// console.log(person[year]);
+// console.log(person.year);
+
+// const keyTag = 'Name';
+// console.log(person['first' + keyTag]);
+
+// modifing objects
+// updating values;
+// console.log(person.birthYear);
+// person.birthYear = 1978;
+// console.log(person.birthYear);
+
+// adding property
+// console.log(person.midName);
+// person.midName = 'Alexis';
+// console.log(person.midName);
+
+// // be carefull for typos
+// person.lasName = 'Brown';
+// console.log(person);
+
+// deleting
+// console.log(person.lastName);
+// delete person.lastName;
+// console.log(person.lastName);
+
+// looping on objects
+// for (let item of Object.values(person)) {
+//   console.log(item);
+// }
+
