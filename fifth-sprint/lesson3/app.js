800e732dda65a3bc1913c83f76ffed1a2c365bbf
israfil <israfil3019@gmail.com>
2021-04-23 15:25:29 +0300

    add filter and reduce func

--- fifth-sprint/lesson3/app.js
+++ fifth-sprint/lesson3/app.js
@@ -43,48 +43,85 @@
 //   // console.log(arr);
 // });
 
-map()
+// map()
 
-const array1 = [1, 4, 9, 16];
+// const array1 = [1, 4, 9, 16];
 
-const map1 = array1.map((item) => item * 2);
+// const map1 = array1.map((item) => item * 2);
 
-console.log(map1);
+// console.log(map1);
 
-// Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'
+// // Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'
 
-const str = 'The Quick Brown Fox';
-const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-console.log(
-  str
-    .split('')
-    .map((letter) =>
-      UPPER.indexOf(letter) !== -1 ? letter.toLowerCase() : letter.toUpperCase()
-    )
-    .join('')
-);
-const swapCase = function(letters){
-    let newLetters = "";
-    for(let i = 0; i<letters.length; i++){
-        if(letters[i] === letters[i].toLowerCase()){
-            newLetters += letters[i].toUpperCase();
-        }else {
-            newLetters += letters[i].toLowerCase();
-        }
-    }
-    console.log(newLetters);
-    return newLetters;
-}
+// const str = 'The Quick Brown Fox';
+// const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// console.log(
+//   str
+//     .split('')
+//     .map((letter) =>
+//       UPPER.indexOf(letter) !== -1 ? letter.toLowerCase() : letter.toUpperCase()
+//     )
+//     .join('')
+// );
+// const swapCase = function(letters){
+//     let newLetters = "";
+//     for(let i = 0; i<letters.length; i++){
+//         if(letters[i] === letters[i].toLowerCase()){
+//             newLetters += letters[i].toUpperCase();
+//         }else {
+//             newLetters += letters[i].toLowerCase();
+//         }
+//     }
+//     console.log(newLetters);
+//     return newLetters;
+// }
 
-const text = 'So, today we have REALLY good day';
+// const text = 'So, today we have REALLY good day';
 
-const swappedText = swapCase(text); 
+// const swappedText = swapCase(text); 
 
-console.log(text.replace(/\w{1}/g, val =>
-    val === val.toLowerCase() ? val.toUpperCase() : val.toLowerCase()));
+// console.log(text.replace(/\w{1}/g, val =>
+//     val === val.toLowerCase() ? val.toUpperCase() : val.toLowerCase()));
 
-console.log(text.split("").map(l=>l==l.toLowerCase()?l.toUpperCase():l.toLowerCase()).join(""));
+// console.log(text.split("").map(l=>l==l.toLowerCase()?l.toUpperCase():l.toLowerCase()).join(""));
 
-console.log(str.split("").map(i => i==i.toLowerCase() ? i.toUpperCase() : i.toLowerCase()).join(""))
+// console.log(str.split("").map(i => i==i.toLowerCase() ? i.toUpperCase() : i.toLowerCase()).join(""))
 
 
+filter ()
+
+const words = [
+  'sprayyyyyyy',
+  'limit',
+  'elite',
+  'exuberant',
+  'destruction',
+  'present',
+];
+
+const result = words.map((word) => (word.length > 6 ? word : 'shorter'));
+
+const result1 = words.filter((word) => word.length > 6);
+console.log(result1);
+
+// reduce ()
+
+const arr1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(arr1.reduce(reducer));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(arr1.reduce(reducer, 5));
+
+let myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd'];
+let unique = myArray.reduce(function (acc, item) {
+  if (acc.indexOf(item) === -1) {
+    acc.push(item);
+  }
+  return acc;
+}, []);
+
+console.log(unique);
