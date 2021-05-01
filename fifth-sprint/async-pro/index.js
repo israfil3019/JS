a54235364c36b7564b9147b56cb4259d963a3e12
israfil <israfil3019@gmail.com>
2021-05-01 15:56:33 +0300

    add callback with right queu

--- fifth-sprint/async-pro/index.js
+++ fifth-sprint/async-pro/index.js
@@ -3,10 +3,6 @@
 // console.timeLog('alert_ornek');
 // console.log('devam eden işler...');
 // console.timeEnd('alert_ornek');
-// const startTime = Date.now();
-// const myLog = (input) => {
-//   console.log(`Elapsed: ${Date.now() - startTime}ms\t\t${input}`);
-// };
 
 
 // const btn = document.querySelector('button');
@@ -16,7 +12,7 @@
   
   //   let pElem = document.createElement('p');
   //   pElem.innerHTML = `This is newly-added paragraph<br>${result}`;
-//   document.body.appendChild(pElem);
+  //   document.body.appendChild(pElem);
 // });
 // setTimeout(() => {
   //   console.log('time ended');
@@ -24,26 +20,67 @@
   
   // console.log('time started');
   // console.log(slowTask());
-  const slowTask = () => {
-    let i = 0;
-    do {
-      i++;
-    } while (i < 700000000);
-    return 'task is completed.';
-  };
+//   const slowTask = () => {
+  //     let i = 0;
+//     do {
+  //       i++;
+  //     } while (i < 700000000);
+//     return 'task is completed.';
+//   };
+
+//   const btn = document.querySelector('button');
+//   btn.addEventListener('click', () => {
+//     setTimeout(() => {
+  //     console.log(slowTask());
+  //   }, 0);
+//   console.log('task done');
+//   let pElem = document.createElement('p');
+//   pElem.innerHTML = `This is newly-added paragraph`;
+//   document.body.appendChild(pElem);
+// });
+
+// btn.addEventListener("click", function myTask(){
+  //   console.log("just it");
+  // });
   
-  const btn = document.querySelector('button');
-  btn.addEventListener('click', () => {
-    setTimeout(() => {
-    console.log(slowTask());
-  }, 0);
-  console.log('task done');
-  let pElem = document.createElement('p');
-  pElem.innerHTML = `This is newly-added paragraph`;
-  document.body.appendChild(pElem);
-});
+const startTime = Date.now();
+const myLog = (input) => {
+  console.log(`Elapsed: ${Date.now() - startTime}ms\t\t${input}`);
+};
 
-btn.addEventListener("click", function myTask(){
-  console.log("just it");
-});
+function brewTea() {
+  /* --- */
+  return 'brewTea done';
+}
 
+function prepareBreakfast() {
+  /* --- */
+return 'prepareBreakfast done';
+}
+
+function makeBreakfast() {
+  /* --- */
+  return 'makeBreakfast done';
+}
+
+function drinkTea() {
+  /* --- */
+  return 'drinkTea done';
+}
+
+function breakfastTime() {
+  myLog(makeBreakfast());
+  myLog(drinkTea());
+  myLog(' all finished');
+}
+
+setTimeout(() => {
+    myLog(brewTea());
+    setTimeout(breakfastTime, 0);
+}, 2000);
+setTimeout(() => {
+    myLog(prepareBreakfast());
+}, 1999);
+for (let index = 0; index < 10; index++) {
+  console.log('object');
+}
