137ecfeb77bd60fb289ea8c45db7bf9cdfea3d09
israfil <israfil3019@gmail.com>
2021-05-01 13:43:43 +0300

    add setTimeout to see delay

--- fifth-sprint/async-pro/index.js
+++ fifth-sprint/async-pro/index.js
@@ -3,16 +3,47 @@
 // console.timeLog('alert_ornek');
 // console.log('devam eden işler...');
 // console.timeEnd('alert_ornek');
-const startTime = Date.now();
-const myLog = (input) => {
-  console.log(`Elapsed: ${Date.now() - startTime}ms\t\t${input}`);
-};
+// const startTime = Date.now();
+// const myLog = (input) => {
+//   console.log(`Elapsed: ${Date.now() - startTime}ms\t\t${input}`);
+// };
 
-const slowTask = () => {
-  let i = 0;
-  do {
-    i++;
-  } while (i < 700000000);
-  return 'task is completed.';
-};
+
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', () => {
+  //   const result = slowTask();
+  //   console.log(result);
+  
+  //   let pElem = document.createElement('p');
+  //   pElem.innerHTML = `This is newly-added paragraph<br>${result}`;
+//   document.body.appendChild(pElem);
+// });
+// setTimeout(() => {
+  //   console.log('time ended');
+  // }, 0);
+  
+  // console.log('time started');
+  // console.log(slowTask());
+  const slowTask = () => {
+    let i = 0;
+    do {
+      i++;
+    } while (i < 700000000);
+    return 'task is completed.';
+  };
+  
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', () => {
+    setTimeout(() => {
+    console.log(slowTask());
+  }, 0);
+  console.log('task done');
+  let pElem = document.createElement('p');
+  pElem.innerHTML = `This is newly-added paragraph`;
+  document.body.appendChild(pElem);
+});
+
+btn.addEventListener("click", function myTask(){
+  console.log("just it");
+});
 
