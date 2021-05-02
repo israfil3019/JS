a16883c73ded60cb7a7ec3d5ad89fd996d250f76
israfil <israfil3019@gmail.com>
2021-05-03 02:29:52 +0300

    add brewTeea func with promise

--- fifth-sprint/async-pro/promise/index.js
+++ fifth-sprint/async-pro/promise/index.js
@@ -3,13 +3,6 @@ const myLog = (input) => {
   console.log(`Elapsed: ${Date.now() - startTime}ms\t\t${input}`);
 };
 
-// const slowTask = () => {
-//   let i = 0;
-//   do {
-//     i++;
-//   } while (i < 800000000);
-//   return 'task is completed.';
-// };
 
 // /* -------- */
 
@@ -23,45 +16,98 @@ const myLog = (input) => {
 // }, 0);
 
 // Promise.resolve().then(() => {
-//   slowTask();
-//   myLog('3. Promise');
-// });
+  //   slowTask();
+  //   myLog('3. Promise');
+  // });
+  
+  // myLog('4. Synchronous');
 
-// myLog('4. Synchronous');
-
-// const myPromise = new Promise((resolve, reject) => {
-//   myLog("promise started");
+  // const myPromise = new Promise((resolve, reject) => {
+    //   myLog("promise started");
 //   /* ---- */
 //   resolve("task completed");
 // });
 
 // myPromise
 //   .then((msg) => {
-//     myLog(msg);
+  //     myLog(msg);
+  //   })
+  //   .catch((err) => {
+    //     myLog(err);
+    //   });
+
+    // const myPromise = new Promise((resolve, reject) => {
+      //   myLog("promise started");
+//   let condition = !Math.floor(Math.random() * 2);
+
+//   if (condition) {
+//     /* ---- */
+//     let result = 42;
+//     resolve(result);
+//   } else {
+//     reject(Error("promise failed"));
+//   }
+//   /* ---- */
+// });
+
+// myPromise
+//   .then((msg) => {
+  //     myLog(msg);
 //   })
 //   .catch((err) => {
-//     myLog(err);
-//   });
+  //     myLog(err);
+  //   });
 
-const myPromise = new Promise((resolve, reject) => {
-  myLog("promise started");
-  let condition = !Math.floor(Math.random() * 2);
+  
 
-  if (condition) {
-    /* ---- */
-    let result = 42;
-    resolve(result);
-  } else {
-    reject(Error("promise failed"));
-  }
-  /* ---- */
-});
+const slowTask = () => {
+  let i = 0;
+  do {
+    i++;
+  } while (i < 800000000);
+  return 'task is completed.';
+};
 
-myPromise
-  .then((msg) => {
-    myLog(msg);
-  })
-  .catch((err) => {
-    myLog(err);
+function brewTea() {
+  return boilWater()
+  .then((status1) => {
+      myLog(status1);
+      return addTea();
+    })
+    .then((status2) => {
+      myLog(status2);
+      slowTask();
+      return 'Enjoy your tea';
+    })
+    .then((status3) => {
+      myLog(status3);
+    })
+    .catch((err) => {
+      myLog(err);
+    });
+}
+
+function boilWater() {
+  return new Promise((resolve, reject) => {
+    const waterBoiled = !Math.floor(Math.random() * 2);
+    if (waterBoiled) {
+      slowTask();
+      resolve('Water Boiled');
+    } else {
+      reject(new Error('kettle is out of order'));
+    }
   });
+}
 
+function addTea() {
+  return new Promise((resolve, reject) => {
+    const haveTea = 0;
+    if (haveTea) {
+      resolve('tea added');
+    } else {
+      reject(new Error('run out of tea'));
+    }
+  });
+}
+
+brewTea();
