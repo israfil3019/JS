76bf3eceb4baeda03d8b912edb27d065c9a83f24
israfil <israfil3019@gmail.com>
2021-05-03 02:12:04 +0300

    use resolve and reject

--- fifth-sprint/async-pro/promise/index.js
+++ fifth-sprint/async-pro/promise/index.js
@@ -1,32 +1,67 @@
-
 const startTime = Date.now();
 const myLog = (input) => {
   console.log(`Elapsed: ${Date.now() - startTime}ms\t\t${input}`);
 };
 
-const slowTask = () => {
-  let i = 0;
-  do {
-    i++;
-  } while (i < 800000000);
-  return 'task is completed.';
-};
+// const slowTask = () => {
+//   let i = 0;
+//   do {
+//     i++;
+//   } while (i < 800000000);
+//   return 'task is completed.';
+// };
 
-/* -------- */
+// /* -------- */
 
-myLog(slowTask());
+// myLog(slowTask());
 
-myLog('1. Synchronous');
+// myLog('1. Synchronous');
 
-setTimeout(() => {
-  slowTask();
-  myLog('2. Timeout');
-}, 0);
+// setTimeout(() => {
+//   slowTask();
+//   myLog('2. Timeout');
+// }, 0);
 
-Promise.resolve().then(() => {
-  slowTask();
-  myLog('3. Promise');
+// Promise.resolve().then(() => {
+//   slowTask();
+//   myLog('3. Promise');
+// });
+
+// myLog('4. Synchronous');
+
+// const myPromise = new Promise((resolve, reject) => {
+//   myLog("promise started");
+//   /* ---- */
+//   resolve("task completed");
+// });
+
+// myPromise
+//   .then((msg) => {
+//     myLog(msg);
+//   })
+//   .catch((err) => {
+//     myLog(err);
+//   });
+
+const myPromise = new Promise((resolve, reject) => {
+  myLog("promise started");
+  let condition = !Math.floor(Math.random() * 2);
+
+  if (condition) {
+    /* ---- */
+    let result = 42;
+    resolve(result);
+  } else {
+    reject(Error("promise failed"));
+  }
+  /* ---- */
 });
 
-myLog('4. Synchronous');
+myPromise
+  .then((msg) => {
+    myLog(msg);
+  })
+  .catch((err) => {
+    myLog(err);
+  });
 
