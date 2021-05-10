b6f7db84843cc48b45f56a1a9f626be27178e835
israfil <israfil3019@gmail.com>
2021-05-10 22:33:49 +0300

    use promise and settime in flipcoin function

--- fifth-sprint/async-pro/async-await/app.js
+++ fifth-sprint/async-pro/async-await/app.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable arrow-body-style */
 
-const renderCountry = (data, className = '') => {
-  const countryElm = document.querySelector('.countries');
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countryElm.insertAdjacentHTML('beforeend', html);
-  countryElm.style.opacity = 1;
-};
+// const renderCountry = (data, className = '') => {
+//   const countryElm = document.querySelector('.countries');
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//   `;
+//   countryElm.insertAdjacentHTML('beforeend', html);
+//   countryElm.style.opacity = 1;
+// };
 
 // // AJAX Calls
 // // XMLHttpRequest - XHR
@@ -103,39 +103,91 @@ const renderCountry = (data, className = '') => {
 // };
 
 
-const getCountryJSONData = (url, errorMsg = 'Something went wrong') => {
-  return fetch(url).then((response) => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+// const getCountryJSONData = (url, errorMsg = 'Something went wrong') => {
+//   return fetch(url).then((response) => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-    return response.json();
+//     return response.json();
+//   });
+// };
+
+
+// const getCountryNeighbourData = (country) => {
+//   getCountryJSONData(
+//     `https://restcountries.eu/rest/v2/name/${country}`,
+//     'No such country'
+//   )
+//     .then((data) => {
+//       console.log(data);
+//       renderCountry(data[0]);
+
+//       const neighbour = data[0].borders[0];
+//       console.log(neighbour);
+//       return getCountryJSONData(
+//         `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+//         'No Neighbour'
+//       );
+//     })
+
+//     .then((data) => {
+//       renderCountry(data, 'neighbour');
+//     })
+//     .catch((err) => console.log(err.message));
+// };
+
+
+
+// getCountryNeighbourData('new zealand');
+
+// building promise
+
+// const flipCoinPromise = new Promise((resolve, reject) => {
+//   console.log('Welcome to toss game');
+//   console.log('Flipping the coin');
+//   setTimeout(() => {
+//     Math.random() >= 0.5
+//       ? resolve('Here Head Comes! You Win!')
+//       : reject(new Error('Tail comes'));
+//   }, 5000);
+// });
+
+// flipCoinPromise
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err.message));
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+
+
+const wait = (seconds) => {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('4 second passed');
+    return wait(1);
   });
-};
-
-
-const getCountryNeighbourData = (country) => {
-  getCountryJSONData(
-    `https://restcountries.eu/rest/v2/name/${country}`,
-    'No such country'
-  )
-    .then((data) => {
-      console.log(data);
-      renderCountry(data[0]);
-
-      const neighbour = data[0].borders[0];
-      console.log(neighbour);
-      return getCountryJSONData(
-        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
-        'No Neighbour'
-      );
-    })
-
-    .then((data) => {
-      renderCountry(data, 'neighbour');
-    })
-    .catch((err) => console.log(err.message));
-};
-
-
-
-getCountryNeighbourData('new zealand');
-
